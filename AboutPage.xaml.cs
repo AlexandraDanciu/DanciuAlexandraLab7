40480b16a58eb9e4db5fd3b83159abd2a37a50fe
ALEXANDRA-IOANA DANCIU <alexandra.ioana.danciu@stud.ubbcluj.ro>
2024-11-18 16:25:05 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace DanciuAlexandraLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DanciuAlexandraLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
